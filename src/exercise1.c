d2e574da5a0ab48c1b1abb100b36623268c2e55c
Roman69l <monstersexygirl@gmail.com>
2025-10-24 09:38:38 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -6,9 +6,7 @@ int main() {
     double array[SIZE];
     double sum = 0.0;
     double result;
-    printf("Введите вещественных чисел: %d\n", SIZE);
     for (int i = 0; i < SIZE; i++) {
-        printf("Число %d: ", i + 1);
         scanf("%lf", &array[i]);
         sum += array[i];
     }
