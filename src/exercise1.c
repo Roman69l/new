aab82d7abbc0fd062a383453cf2f5fa92c225f00
Roman69l <monstersexygirl@gmail.com>
2025-10-24 09:34:50 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+#define SIZE 10
+
+int main() {
+    double array[SIZE];
+    double sum = 0.0;
+    double result;
+    printf("Введите вещественных чисел: %d\n", SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        printf("Число %d: ", i + 1);
+        scanf("%lf", &array[i]);
+        sum += array[i];
+    }
+    result = sum / SIZE;
+    printf("Среднее арифметическое: %.3f\n", result);
     return 0;
 }
+
